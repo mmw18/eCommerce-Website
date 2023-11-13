c3999ed88d64995fe1112c09dc2c88745a44517e
Megan Wright <meganmwright2018@gmail.com>
2023-11-12 21:43:06 -0600

    Adding code to some routes and server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +7,29 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use(routes); 
 
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+
+
+
+
+// const express = require('express');
+// const routes = require('./routes');
+// // import sequelize connection
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(routes);
+
+// // sync sequelize models to the database, then turn on the server
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
