c8c5ed96182fce6c7ace6cd475728a48ce7c2b06
Megan Wright <meganmwright2018@gmail.com>
2023-11-23 19:14:04 -0600

    Changes in server and packages

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./connection');
+const sequelize = require('./sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
